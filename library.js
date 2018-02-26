c0b930744165a6e6ff1cc474a9571c177f385ab2
Moiz Hasan <Moiz_Hasan@yahoo.com>
2018-02-26 02:41:59 -0600

    added render method and listeners

--- library.js
+++ library.js
@@ -1,24 +1,44 @@
-let myLibrary = [book1]
 
-function Book(title, author, pages) {
+const book1 = new Book('Starship Troopers', 'Robert Heinlein', '150');
+//console.log(book1.info())
+
+let myLibrary = [book1];
+
+function Book(title, author, pages, read = false) {
   this.title = title
   this.author = author
   this.pages = pages
-  this.read = false
+  this.read = read
   
   this.info = function() {
-    return this.title + 'by ' + this.author + this.pages + this.read
+  	var readString = this.read ? "have read" : "not read";
+
+    return '<ul class="book"><li><h2>' + this.title + ' </h2></li><li><h4>' + this.author +
+  	'</h4></li><li><h4>' + this.pages + ' pages</h4></li><li><h4>' + readString + '</h4></li></ul>';
   }
+
 }
 
+
 function addBookToLibrary() {
-  // do stuff here
+  var book = new Book('', )
 }
 
 //add our books to the html
 function render() {
-  
+	var books = '';
+  for (var i = 0; i < myLibrary.length; i++) {
+  	books += myLibrary[i].info();
+  }
+
+  var library = document.getElementById('library').innerHTML = books;
 }
 
-const book1 = new Book('Starship Troopers', 'Robert Heinlein', '150')
-//console.log(book1.info())
+function showForm() {
+	document.getElementById("bookDetails").style.display = "block";
+}
+
+
+document.getElementById("addBookButton").addEventListener("click", showForm);
+document.getElementById("bookSubmit").addEventListener("click", addBookToLibrary);
+render();
